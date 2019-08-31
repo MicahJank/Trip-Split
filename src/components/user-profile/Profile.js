3dab096b829c229425083907eccbc1b5b617ec06
MicahJank <MicahJank@users.noreply.github.com>
2019-08-30 21:26:58 -0500

    styled users profile page more

--- src/components/user-profile/Profile.js
+++ src/components/user-profile/Profile.js
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 
-import { Button } from 'semantic-ui-react';
+import { Button, Image, Icon, Menu } from 'semantic-ui-react';
+
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 800px;
+
+    padding: 30px;
+
+    i {
+        font-size: 7rem;
+        cursor: pointer;
+    }
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    button.log-out {
+        width: 200px;
+    }
+
+`;
 
 const Profile = ({history}) => {
 
@@ -11,10 +38,16 @@ const Profile = ({history}) => {
     };
 
     return (
-        <>
-        <h1>Users Profile</h1>
-        <Button primary onClick={clearStorage}>Log Out</Button>
-        </>
+        <Container>
+            <Icon color='grey' name='user' circular={true} />
+            <h1>Users Name</h1>
+            <Button.Group size='large' className='settings' vertical>
+                <Button className='tab' content='Settings' />
+                <Button className='tab' content='User Preferences' />
+                <Button className='tab' content='Contact Us' />
+            </Button.Group>
+            <Button className='log-out' fluid={false} primary onClick={clearStorage}>Log Out</Button>
+        </Container>
     );
 };
 
